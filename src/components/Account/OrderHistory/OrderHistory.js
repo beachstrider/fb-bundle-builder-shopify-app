62077880f6d3caf0733c67d765f1853dee7068a3
Justin <justin@sunriseintegration.com>
2021-11-01 14:23:47 -0700

    Update ui for order history

--- src/components/Account/OrderHistory/OrderHistory.js
+++ src/components/Account/OrderHistory/OrderHistory.js
@@ -10,9 +10,10 @@ const OrderHistory = () => {
     <div className="contentWrapper">
         <div className="bundleRow">
             <div className="bundleOneThird">
+                <p></p>
             </div>
             <div className="bundleTwoThirds">
-                <div>
+                <div className="headerOffset">
                     <h1>Order History</h1>
                 </div>
             </div>
@@ -23,23 +24,47 @@ const OrderHistory = () => {
             </div>
             <div className="bundleTwoThirds">
                 <div className="bundleBuilderCard">
-                    <div className={styles.currentOrderWrapper}>
-                        <h3>Current Order</h3>
+                    <div className={styles.contentCardWrapper}>
+                        <div className={styles.contentCardNavigation}>
+                            <h3>Current Order</h3>
+                            <Link to="/account" className="secondaryButton">Edit Order</Link>
+                        </div>
                         <div className={styles.currentOrderMenu}>
-                            <MenuItemCard />
-                            <MenuItemCard />
-                            <MenuItemCard />
-                            <div className={styles.viewMoreLink}>
-                                <Link className={styles.viewMoreItemsLink} to="#">
-                                    See All
-                                </Link>
-                            </div>
+                            <MenuItemCard width="27%" />
+                            <MenuItemCard width="27%" />
+                            <MenuItemCard width="27%" />
+                            <Link to="/account" className={styles.seeAllMenu}>
+                                See All
+                            </Link>
                         </div>
                     </div>
                 </div>
                 <div className="bundleBuilderCard">
                     <div className={styles.currentOrderWrapper}>
-                        <h3>Past Orders</h3>
+                        <div className={styles.contentCardNavigation}>
+                            <h3>Past Orders</h3>
+                            <p></p>
+                        </div>
+                        <table className={styles.orderHistoryTable}>
+                            <thead className={styles.orderHistoryTableHeaders}>
+                                <th>Order Date</th>
+                                <th>Order Total</th>
+                                <th>Meal Type</th>
+                                <th>Meals Names</th>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>11/11/2021</td>
+                                    <td>$154.00</td>
+                                    <td>Keto</td>
+                                    <td className={styles.orderMealNames}>
+                                        <p className={styles.orderMealNamesText}>Pink Peppercorn Sirloin, Barbados Sirlion, Another Meal Name and Another to make it long enough.</p>
+                                        <Link to="#" className={styles.orderMealNamesLink}>See All Meals</Link>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                        <Link to="#" className={styles.orderHistoryMoreLink}>See More </Link>
                     </div>
                 </div>
             </div>
