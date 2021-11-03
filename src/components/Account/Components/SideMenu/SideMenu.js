e37920b32f203cf640f7a97f7c101254629b3b06
Justin <justin@sunriseintegration.com>
2021-11-03 15:06:05 -0700

    update components

--- src/components/Account/Components/SideMenu/SideMenu.js
+++ src/components/Account/Components/SideMenu/SideMenu.js
@@ -2,16 +2,35 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import styles from './SideMenu.module.scss'
 
+import {
+  NoteMajor,
+  ProfileMajor,
+  CreditCardMajor,
+  ShipmentMajor
+} from '@shopify/polaris-icons';
+
 const SideMenu = (props) => {
 
   return (
     <div className="bundleBuilderCard">
-        <div className={styles.menuWrapper}>
-            <Link to="/plan-settings" className={props.active === 'plan-settings' ? styles.activeLink : styles.menuLink}>Plan Settings</Link>
-            <Link to="/account-info" className={props.active === 'account-info'? styles.activeLink : styles.menuLink}>Account Info</Link>
-            <Link to="/payment-method" className={props.active === 'payment-method' ? styles.activeLink : styles.menuLink}>Payment Methods</Link>
-            <Link to="/order-history" className={props.active === 'order-history' ? styles.activeLink : styles.menuLink}>OrderHistory</Link>
-        </div>
+      <div className={styles.menuWrapper}>
+        <Link to="/plan-settings" className={props.active === 'plan-settings' ? styles.activeLink : styles.menuLink}>
+          <span className={styles.iconWrapper}><NoteMajor /></span>
+          Plan Settings
+        </Link>
+        <Link to="/account-info" className={props.active === 'account-info' ? styles.activeLink : styles.menuLink}>
+          <span className={styles.iconWrapper}><ProfileMajor /></span>
+          Account Info
+        </Link>
+        <Link to="/payment-method" className={props.active === 'payment-method' ? styles.activeLink : styles.menuLink}>
+          <span className={styles.iconWrapper}><CreditCardMajor /></span>
+          Payment Methods
+        </Link>
+        <Link to="/order-history" className={props.active === 'order-history' ? styles.activeLink : styles.menuLink}>
+          <span className={styles.iconWrapper}><ShipmentMajor /></span>
+          OrderHistory
+        </Link>
+      </div>
     </div>
   )
 }
