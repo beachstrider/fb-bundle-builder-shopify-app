9ecbe9ddae8f89916886c0ba69baf48f55c5a33c
Justin <justin@sunriseintegration.com>
2021-11-08 11:48:41 -0800

    Update to order history and payment details

--- src/components/Account/OrderHistory/OrderHistory.js
+++ src/components/Account/OrderHistory/OrderHistory.js
@@ -1,19 +1,67 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, Redirect } from 'react-router-dom'
 import styles from './OrderHistory.module.scss'
 import {SideMenu} from '../Components/SideMenu'
 import {MenuItemCard} from '../Components/MenuItemCard'
+import {
+    ChevronRightMinor
+  } from '@shopify/polaris-icons';
+
+
+const  orders = [
+    {
+        id: 1,
+        total: '$154.12',
+        type: 'Keto',
+        createdAt: '11/21/2021',
+        orderItems: ['Pink Peppercorn Sirloin', 'Barbados Sirloin', 'Cheese salmon fillet']
+    },
+    {
+        id: 2,
+        total: '$154.12',
+        type: 'Keto',
+        createdAt: '11/21/2021',
+        orderItems: ['Pink Peppercorn Sirloin', 'Barbados Sirloin', 'Cheese salmon fillet']
+    },
+    {
+        id: 3,
+        total: '$154.12',
+        type: 'Keto',
+        createdAt: '11/21/2021',
+        orderItems: ['Pink Peppercorn Sirloin', 'Barbados Sirloin', 'Cheese salmon fillet']
+    },
+    {
+        id: 4,
+        total: '$154.12',
+        type: 'Keto',
+        createdAt: '11/21/2021',
+        orderItems: ['Pink Peppercorn Sirloin', 'Barbados Sirloin', 'Cheese salmon fillet']
+    },
+    {
+        id: 5,
+        total: '$154.12',
+        type: 'Keto',
+        createdAt: '11/21/2021',
+        orderItems: ['Pink Peppercorn Sirloin', 'Barbados Sirloin', 'Cheese salmon fillet']
+    }
+]
 
 const OrderHistory = () => {
+
+    if(shopCustomer.id === 0){
+        return <Redirect push to="/" />
+    }
+    
 
   return (
     <div className="contentWrapper">
         <div className="bundleRow">
             <div className="bundleOneThird">
+                <p></p>
             </div>
             <div className="bundleTwoThirds">
-                <div>
-                    <h1>Order History</h1>
+                <div className="headerOffset">
+                    <h2>Order History</h2>
                 </div>
             </div>
         </div>
@@ -23,23 +71,51 @@ const OrderHistory = () => {
             </div>
             <div className="bundleTwoThirds">
                 <div className="bundleBuilderCard">
-                    <div className={styles.currentOrderWrapper}>
-                        <h3>Current Order</h3>
+                    <div className={styles.contentCardWrapper}>
+                        <div className={styles.contentCardNavigation}>
+                            <h3>Current Order</h3>
+                            <Link to="/account" className="secondaryButton">Edit Order</Link>
+                        </div>
                         <div className={styles.currentOrderMenu}>
-                            <MenuItemCard />
-                            <MenuItemCard />
-                            <MenuItemCard />
-                            <div className={styles.viewMoreLink}>
-                                <Link className={styles.viewMoreItemsLink} to="#">
-                                    See All
-                                </Link>
-                            </div>
+                            <MenuItemCard width="27%" />
+                            <MenuItemCard width="27%" />
+                            <MenuItemCard width="27%" />
+                            <Link to="/account" className={styles.seeAllMenu}>
+                                See All <ChevronRightMinor />
+                            </Link>
                         </div>
                     </div>
                 </div>
                 <div className="bundleBuilderCard">
-                    <div className={styles.currentOrderWrapper}>
-                        <h3>Past Orders</h3>
+                    <div className={styles.contentCardWrapper}>
+                        <div className={styles.contentCardNavigation}>
+                            <h3>Past Orders</h3>
+                            <p></p>
+                        </div>
+                        <table className={styles.orderHistoryTable}>
+                            <thead className={styles.orderHistoryTableHeaders}>
+                                <th>Order Date</th>
+                                <th>Order Total</th>
+                                <th>Meal Type</th>
+                                <th>Meals Names</th>
+                            </thead>
+                            <tbody>
+                                {orders.map( (order, index) => {
+                                    return (
+                                        <tr key={index}>
+                                            <td>{order.createdAt}</td>
+                                            <td>{order.total}</td>
+                                            <td>{order.type}</td>
+                                            <td className={styles.orderMealNames}>
+                                                <p className={styles.orderMealNamesText}>{order.orderItems.map( item => ( item ))}</p>
+                                                <Link to="#" className={styles.orderMealNamesLink}>See All Meals</Link>
+                                            </td>
+                                        </tr>  
+                                    )
+                                })}
+                            </tbody>
+                        </table>
+                        <Link to="#" className={styles.orderHistoryMoreLink}>See More </Link>
                     </div>
                 </div>
             </div>
--- src/components/Account/PaymentMethod/PaymentMethod.js
+++ src/components/Account/PaymentMethod/PaymentMethod.js
@@ -1,17 +1,23 @@
 import React from 'react'
+import { Link, Redirect } from 'react-router-dom'
 import styles from './PaymentMethod.module.scss'
 import {SideMenu} from '../Components/SideMenu'
 
 const PaymentMethod = () => {
 
+    if(shopCustomer.id === 0){
+        return <Redirect push to="/" />
+    }
+
   return (
     <div className="contentWrapper">
         <div className="bundleRow">
             <div className="bundleOneThird">
+                <p></p>
             </div>
             <div className="bundleTwoThirds">
-                <div>
-                    <h1>Payment Method</h1>
+                <div className="headerOffset">
+                    <h2>Payment Method</h2>
                 </div>
             </div>
         </div>
@@ -21,8 +27,16 @@ const PaymentMethod = () => {
             </div>
             <div className="bundleTwoThirds">
                 <div className="bundleBuilderCard">
-                    <div className={styles.currentOrderWrapper}>
-                        <h3>Default Card</h3>
+                    <div className={styles.contentCardWrapper}>
+                        <div className={styles.contentCardNavigation}>
+                            <h3>Default Card</h3>
+                            <a href="#" className="secondaryButton">Edit Info</a>
+                        </div>
+                        <div>
+                            <p><span className={styles.boldTextField}>Name:</span>{shopCustomer.fullName}</p>
+                            <p><span className={styles.boldTextField}>Card Number:</span>***************1234</p>
+                            <div className={styles.accountAddress}><p><span className={styles.boldTextField}>Billing Address:</span></p><p>{shopCustomer.address.street}<br />{shopCustomer.address.city}, {shopCustomer.address.provinceCode} {shopCustomer.address.zip}</p></div>
+                        </div>
                     </div>
                 </div>
             </div>
