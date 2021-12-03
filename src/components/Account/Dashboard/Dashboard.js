a7b728c4882141a53528c9f23bee89a45bec1f39
Justin <justin@sunriseintegration.com>
2021-12-02 16:15:42 -0800

    added global spinner

--- src/components/Account/Dashboard/Dashboard.js
+++ src/components/Account/Dashboard/Dashboard.js
@@ -16,7 +16,7 @@ import {
 import isSameOrAfter from 'dayjs/plugin/isSameOrAfter';
 import * as dayjs from 'dayjs';
 import { request } from '../../../utils';
-import {Spinner} from '@shopify/polaris';
+import { Spinner } from '../../Global';
 
 dayjs.extend(isSameOrAfter);
 
@@ -230,12 +230,7 @@ const Dashboard = () => {
   if (loading) {
     // TODO: work in progress
     return (
-      <div className="customSpinner">
-        <div className="customSpinnerIcon">
-          <svg viewBox="0 0 44 44" xmlns="http://www.w3.org/2000/svg"><path d="M15.542 1.487A21.507 21.507 0 00.5 22c0 11.874 9.626 21.5 21.5 21.5 9.847 0 18.364-6.675 20.809-16.072a1.5 1.5 0 00-2.904-.756C37.803 34.755 30.473 40.5 22 40.5 11.783 40.5 3.5 32.217 3.5 22c0-8.137 5.3-15.247 12.942-17.65a1.5 1.5 0 10-.9-2.863z"></path></svg>
-        </div>
-        <span className="customSpinnerLabel">Page Loading...</span>
-      </div>
+      <Spinner label="Loading..." />
     )
   }
 
