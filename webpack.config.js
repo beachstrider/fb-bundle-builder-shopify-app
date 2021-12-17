eb975635303ced716217feaf7c094f69c688a2c7
Justin <justin@sunriseintegration.com>
2021-12-17 15:14:18 -0800

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,4 @@
-const path = require('path')
-require('dotenv').config({ path: path.resolve(__dirname, '.env') })
+require('dotenv').config()
 const webpack = require('webpack')
 console.log({
   'process.env.LOGO_URL': JSON.stringify(process.env.LOGO_URL),
