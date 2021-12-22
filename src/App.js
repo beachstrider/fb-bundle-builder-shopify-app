0874e27313e749c3e0ef1d5cfa60884f6c9fde54
Eduardo Donoso <eduardo@sunriseintegration>
2021-12-21 17:42:42 -0800

    better tag search logs

--- src/App.js
+++ src/App.js
@@ -30,12 +30,10 @@ import { getSelectedBundle } from './components/Hooks'
 function App() {
   const state = useSelector((state) => state)
 
-  useEffect(() => {
-    // TODO: remove this block of code later (testing purposes)
-    findMissingTags()
+  useEffect(async () => {
+    await findMissingTags()
   }, [])
 
-  // TODO: remove this block of code later (testing purposes)
   const findMissingTags = async () => {
     const expectedTags = [
       '7 Day with breakfast',
@@ -46,33 +44,38 @@ function App() {
       '3 Day'
     ]
 
+    console.log('Searching for Shopify tags in products', expectedTags)
+    const missingTags = [];
     for (const findTag of expectedTags) {
       const currentTag = await getSelectedBundle(findTag)
       if (Object.keys(currentTag).length === 0) {
-        console.log('TAG not found:', findTag)
+        missingTags.push(findTag)
       }
+    }
+    if(missingTags.length){
+      console.error('ERROR: Shopify tags missing from products', missingTags)
     }
   }
 
   return (
     <AppProvider i18n={enTranslations}>
       <Router>
-        <div className="defaultWrapper flexColumnDirection">
+        <div className='defaultWrapper flexColumnDirection'>
           {state.displayHeader && <Header />}
-          <div className="content mb-9">
+          <div className='content mb-9'>
             <Switch>
-              <Route exact path="/account" component={Dashboard} />
-              <Route exact path="/order-history" component={OrderHistory} />
-              <Route exact path="/account-info" component={AccountInfo} />
-              <Route exact path="/payment-method" component={PaymentMethod} />
-              <Route exact path="/plan-settings" component={PlanSettings} />
-              <Route exact path="/edit-order/:orderId" component={EditOrder} />
+              <Route exact path='/account' component={Dashboard} />
+              <Route exact path='/order-history' component={OrderHistory} />
+              <Route exact path='/account-info' component={AccountInfo} />
+              <Route exact path='/payment-method' component={PaymentMethod} />
+              <Route exact path='/plan-settings' component={PlanSettings} />
+              <Route exact path='/edit-order/:orderId' component={EditOrder} />
 
-              <Route path="/steps/2" component={Location} />
-              <Route path="/steps/3" component={EntreeType} />
-              <Route path="/steps/4" component={Entrees} />
-              <Route path="/steps/5" component={Review} />
-              <Route path="*" component={Frequency} />
+              <Route path='/steps/2' component={Location} />
+              <Route path='/steps/3' component={EntreeType} />
+              <Route path='/steps/4' component={Entrees} />
+              <Route path='/steps/5' component={Review} />
+              <Route path='*' component={Frequency} />
             </Switch>
           </div>
           {state.displayFooter && <Footer />}
