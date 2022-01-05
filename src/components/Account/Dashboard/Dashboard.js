b5c381975ab4b9e7c2164c0fe8fd1b781cd1abe6
Arthur S <arthur@sunriseintegration.com>
2022-01-04 17:19:07 -0800

    Fix account managament

--- src/components/Account/Dashboard/Dashboard.js
+++ src/components/Account/Dashboard/Dashboard.js
@@ -97,51 +97,56 @@ const getData = async () => {
         const subscriptionOrders = await request(`${process.env.PROXY_APP_URL}/bundle-api/subscriptions/${sub.id}/orders`, { method: 'get', data: '', headers: { authorization: `Bearer ${token}` }}, 3)
         const configData = await request(`${process.env.PROXY_APP_URL}/bundle-api/bundles/${sub.bundle_id}/configurations`, { method: 'get', data: '', headers: { authorization:`Bearer ${token}` }}, 3)
         if(configData.data.data.length > 0){
-          for( const config of configData.data.data){
+          for( const config of configData.data.data) {
             let subCount = 0;
             for (const content of config.contents) {
               const dayOfTheWeek = dayjs(content.deliver_after.split('T')[0]).day()
               const today = dayjs().day(dayOfTheWeek).format('YYYY-MM-DD');
               const displayDate = dayjs(content.deliver_after.split('T')[0]).format('YYYY-MM-DD');
               const cutoffDate = dayjs().day(sub.delivery_day).add(4, 'day');
-              if(subCount < 3 && dayjs(displayDate).isSameOrAfter(dayjs(today))){
+              
+
+              if(subCount < 4 && dayjs(displayDate).isSameOrAfter(dayjs(today))){
                 const orderedItems = subscriptionOrders.data.data.filter(ord => ord.bundle_configuration_content.deliver_after === content.deliver_after);
                 const subscriptionObjKey = content.deliver_after.split('T')[0]
-                if(!weeksMenu.includes(dayjs(content.deliver_after.split('T')[0]).format('YYYY-MM-DD'))){
-                  // weeksMenu.push(dayjs(content.deliver_after.replace('T00:00:00.000Z', 'T12:00:00.000Z')).format('YYYY-MM-DD'))
-                  weeksMenu.push(dayjs(content.deliver_after).utc().format('YYYY-MM-DD'))
-                  subscriptionArray[subscriptionObjKey] = {}
-                  subscriptionArray[subscriptionObjKey].items = []
-                }
                 
-                if(orderedItems.length > 0){
-                  const orderFound = orderedItems[0]
-                  if(subscriptionArray[subscriptionObjKey]){
-                      const thisItemsArray = await buildProductArrayFromVariant(orderFound.items, sub.subscription_sub_type, shopProducts);
-                      console.log('subscriptionObjKey: ', subscriptionObjKey)
-                      console.log('thisItemsArray: ', thisItemsArray)
-                      subscriptionArray[subscriptionObjKey].subId = sub.id;
-                      subscriptionArray[subscriptionObjKey].deliveryDay = sub.delivery_day;
-                      subscriptionArray[subscriptionObjKey].items = thisItemsArray;
-                      subscriptionArray[subscriptionObjKey].status = orderFound.platform_order_id !== null ? 'sent' : dayjs(content.deliver_after).isSameOrAfter(cutoffDate) ? 'pending' : 'locked';
-                      subscriptionArray[subscriptionObjKey].subscriptionDate = dayjs(subscriptionObjKey).format('YYYY-MM-DD');
-                      subscriptionArray[subscriptionObjKey].queryDate = content.deliver_after
-                      if(orderFound.platform_order_id !== null){
-                        subscriptionArray[subscriptionObjKey].trackingUrl = await getOrderTrackingUrl(orderFound.platform_order_id, shopCustomer);
-                      }
+                if (orderedItems.length !== 0 || subCount !== 0) {
+                  if(!weeksMenu.includes(dayjs(content.deliver_after.split('T')[0]).format('YYYY-MM-DD'))){
+                    // weeksMenu.push(dayjs(content.deliver_after.replace('T00:00:00.000Z', 'T12:00:00.000Z')).format('YYYY-MM-DD'))
+                    weeksMenu.push(dayjs(content.deliver_after).utc().format('YYYY-MM-DD'))
+                    subscriptionArray[subscriptionObjKey] = {}
+                    subscriptionArray[subscriptionObjKey].items = []
                   }
-                } else {
-                  const configContentsData = await request(`${process.env.PROXY_APP_URL}/bundle-api/bundles/${config.bundle_id}/configurations/${config.id}/contents/${content.id}/products?is_default=1`, { method: 'get', data: '', headers: { authorization: `Bearer ${token}` }}, 3)
-                  const thisProductsArray = await buildProductArrayFromId(configContentsData.data.data, sub.subscription_sub_type, shopProducts);
-                  console.log('subscriptionObjKey: ', subscriptionObjKey)
-                  console.log('thisProductsArray: ', thisProductsArray)
-                  
-                  subscriptionArray[subscriptionObjKey].subId = sub.id;
-                  subscriptionArray[subscriptionObjKey].items = subscriptionArray[subscriptionObjKey].items.concat(thisProductsArray);
-                  subscriptionArray[subscriptionObjKey].status = dayjs(content.deliver_after).isSameOrAfter(cutoffDate) ?  'pending' : 'locked';
-                  subscriptionArray[subscriptionObjKey].subscriptionDate = dayjs(subscriptionObjKey).format('YYYY-MM-DD')
-                  subscriptionArray[subscriptionObjKey].queryDate = content.deliver_after
+                  if(orderedItems.length > 0) {
+                    const orderFound = orderedItems[0]
+                    if(subscriptionArray[subscriptionObjKey]){
+                        const thisItemsArray = await buildProductArrayFromVariant(orderFound.items, sub.subscription_sub_type, shopProducts);
+                        console.log('subscriptionObjKey: ', subscriptionObjKey)
+                        console.log('thisItemsArray: ', thisItemsArray)
+                        subscriptionArray[subscriptionObjKey].subId = sub.id;
+                        subscriptionArray[subscriptionObjKey].deliveryDay = sub.delivery_day;
+                        subscriptionArray[subscriptionObjKey].items = subscriptionArray[subscriptionObjKey].items.concat(thisItemsArray);
+                        subscriptionArray[subscriptionObjKey].status = orderFound.platform_order_id !== null ? 'sent' : dayjs(content.deliver_after).isSameOrAfter(cutoffDate) ? 'pending' : 'locked';
+                        subscriptionArray[subscriptionObjKey].subscriptionDate = dayjs(subscriptionObjKey).format('YYYY-MM-DD');
+                        subscriptionArray[subscriptionObjKey].queryDate = content.deliver_after
+                        if(orderFound.platform_order_id !== null){
+                          subscriptionArray[subscriptionObjKey].trackingUrl = await getOrderTrackingUrl(orderFound.platform_order_id, shopCustomer);
+                        }
+                    }
+                  } else {
+                    const configContentsData = await request(`${process.env.PROXY_APP_URL}/bundle-api/bundles/${config.bundle_id}/configurations/${config.id}/contents/${content.id}/products?is_default=1`, { method: 'get', data: '', headers: { authorization: `Bearer ${token}` }}, 3)
+                    const thisProductsArray = await buildProductArrayFromId(configContentsData.data.data, sub.subscription_sub_type, shopProducts);
+                    console.log('subscriptionObjKey: ', subscriptionObjKey)
+                    console.log('thisProductsArray: ', thisProductsArray)
+                    
+                    subscriptionArray[subscriptionObjKey].subId = sub.id;
+                    subscriptionArray[subscriptionObjKey].items = subscriptionArray[subscriptionObjKey].items.concat(thisProductsArray);
+                    subscriptionArray[subscriptionObjKey].status = dayjs(content.deliver_after).isSameOrAfter(cutoffDate) ?  'pending' : 'locked';
+                    subscriptionArray[subscriptionObjKey].subscriptionDate = dayjs(subscriptionObjKey).format('YYYY-MM-DD')
+                    subscriptionArray[subscriptionObjKey].queryDate = content.deliver_after
 
+                  }
+                  
                 }
                 subCount++
               }
@@ -243,7 +248,7 @@ const getData = async () => {
           <div key={idx} className={styles.subscriptionRow}>
             <div className={styles.menuRow}>
               <div className={styles.headerWthLink}>
-                <h3>Week of {dayjs(sub.subscriptionDate).format('MMM DD')}</h3>
+                <h3>Delivering Week of {dayjs(sub.subscriptionDate).format('MMM DD')}</h3>
                 {sub.status === 'sent' ? <a href={sub.trackingUrl} className={styles.primaryLink}>Track Package</a> : ''}
               </div>
               {sub.status === 'sent' || sub.status === 'locked' ? <Link to={`/order-history?date=${sub.queryDate}`} className="secondaryButton">Order Summary</Link>  : <Link to={`/edit-order/${sub.subId}?date=${sub.queryDate}`} className="secondaryButton">Edit Order</Link>}
