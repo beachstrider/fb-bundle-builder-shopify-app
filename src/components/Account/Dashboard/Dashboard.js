e15ae910f472378e045d96254c6f3daefe95cc27
Arthur S <arthur@sunriseintegration.com>
2022-01-18 11:51:08 -0800

    Clear token and validate email on the dashboard

--- src/components/Account/Dashboard/Dashboard.js
+++ src/components/Account/Dashboard/Dashboard.js
@@ -4,7 +4,9 @@ import {
   displayHeader,
   displayFooter,
   selectFaqType,
-  setTokens
+  setTokens,
+  reset,
+  setEmail
 } from '../../../store/slices/rootSlice'
 import { Link, Redirect, useLocation, useHistory } from 'react-router-dom'
 import styles from './Dashboard.module.scss'
@@ -26,6 +28,7 @@ import {
   uniqueArray
 } from '../../../utils'
 import { Spinner } from '../../Global'
+import { clearLocalStorage } from '../../../store/store'
 
 import Toast from '../../Global/Toast'
 
@@ -68,13 +71,24 @@ const Dashboard = () => {
     getData()
   }, [])
 
+  const clearState = () =>
+    new Promise((resolve) => {
+      setTimeout(() => {
+        clearLocalStorage()
+        dispatch(reset())
+        resolve('ok')
+      }, 1000)
+    })
+
   const getData = async () => {
-    if (!state.tokens.userToken) {
-      const thisToken = await getToken()
-      await getOrdersToShow(thisToken)
+    if (shopCustomer.email !== state.email || !state.tokens.userToken) {
+      const userToken = await getToken()
+      await clearState()
+      await getOrdersToShow(userToken)
     } else {
       await getOrdersToShow(state.tokens.userToken)
     }
+    dispatch(setEmail(shopCustomer?.email || ''))
   }
 
   const getToken = async () => {
