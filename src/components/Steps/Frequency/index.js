860701345acb458bd7f2a82eda3985d44f19b5bf
Arthur S <arthur@sunriseintegration.com>
2022-02-07 20:02:07 -0800

    Create new checkmark card
    Change background
    
    Change card border radius
    
    remove children styling
    
    Increase card border size
    
    Change meals and breakfast selection layout
    
    Change subtotal component
    
    Desktop and table view working
    
    Finish mobile styling
    
    Change subTotal card width
    
    Remove meals and breakfast calculation

--- src/components/Steps/Frequency/index.js
+++ src/components/Steps/Frequency/index.js
@@ -1,17 +1,5 @@
 import Frequency from './Frequency'
-import FrequencySubTotalItem from './FrequencySubTotalItem'
-import FrequencyWeeklyPrice from './FrequencyWeeklyPrice'
 import FrequencyEntree from './FrequencyEntree'
-import FrequencyMainEntree from './FrequencyMainEntree'
 import FrequencyBreakfast from './FrequencyBreakfast'
-import FrequencySubTotal from './FrequencySubTotal'
 
-export {
-  Frequency,
-  FrequencyBreakfast,
-  FrequencyEntree,
-  FrequencyMainEntree,
-  FrequencySubTotal,
-  FrequencySubTotalItem,
-  FrequencyWeeklyPrice
-}
+export { Frequency, FrequencyBreakfast, FrequencyEntree }
