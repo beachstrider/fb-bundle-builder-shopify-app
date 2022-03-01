ebe19f28f67be83d1d26f6f2750c6c77d4f379c7
Eduardo Donoso <eduardo@sunriseintegration>
2022-02-28 17:28:20 -0800

    add logs to getselectedbundle

--- src/components/Hooks/getSelectedBundle.js
+++ src/components/Hooks/getSelectedBundle.js
@@ -2,6 +2,7 @@ const getSelectedBundle = (tag) => {
   try {
     let currentBundle = {}
     shopBundles.forEach((bundle) => {
+      console.log('getSelectedBundle', tag.toLowerCase(), bundle)
       const findTag = bundle.tags.find((t) => t.toLowerCase() === tag.toLowerCase())
       if (findTag) {
         currentBundle = bundle
