5b1a763b8359ee3a2a502421a21cab0fe8be4319
Eduardo Donoso <eduardo@sunriseintegration.com>
2022-04-07 14:28:31 -0700

    Revert "Adding server side rendering"

--- src/index.js
+++ src/index.js
@@ -4,26 +4,24 @@ import App from './App'
 import { Provider } from 'react-redux'
 import store from './store/store'
 // Sentry
-import * as Sentry from '@sentry/react'
-import { BrowserTracing } from '@sentry/tracing'
+import * as Sentry from "@sentry/react"
+import { BrowserTracing } from "@sentry/tracing"
 import { createBrowserHistory } from 'history'
 
-const history = createBrowserHistory()
+const history = createBrowserHistory();
 
-console.log(process.env.SENTRY_DSN, process.env.SENTRY_ENVIRONMENT)
+console.log(process.env.SENTRY_DSN, process.env.SENTRY_ENVIRONMENT);
 Sentry.init({
   environment: process.env.SENTRY_ENVIRONMENT,
   dsn: process.env.SENTRY_DSN,
-  integrations: [
-    new BrowserTracing({
-      tracingOrigins: ['localhost', process.env.BUNDLE_API_URL, /^\//],
-      routingInstrumentation: Sentry.reactRouterV5Instrumentation(history)
-    })
-  ],
+  integrations: [new BrowserTracing({
+    tracingOrigins: ["localhost", process.env.BUNDLE_API_URL, /^\//],
+    routingInstrumentation: Sentry.reactRouterV5Instrumentation(history),
+  })],
   tracesSampleRate: process.env.SENTRY_SAMPLE_RATE
 })
 
-ReactDOM.hydrate(
+ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
