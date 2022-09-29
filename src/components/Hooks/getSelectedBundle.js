fa0691f53be1c7618b0d00308f3a027c00fef8e9
Kalam <ojkalam@gmail.com>
2022-09-30 02:51:27 +0600

    set 9 meal plan as default in qf,etp,chow

--- src/components/Hooks/getSelectedBundle.js
+++ src/components/Hooks/getSelectedBundle.js
@@ -2,7 +2,7 @@ const getSelectedBundle = (tag) => {
   try {
     let currentBundle = {}
     shopBundles.forEach((bundle) => {
-      console.log('getSelectedBundle', tag.toLowerCase(), bundle)
+      // console.log('getSelectedBundle', tag.toLowerCase(), bundle)
       const findTag = bundle.tags.find(
         (t) => t.toLowerCase() === tag.toLowerCase()
       )
